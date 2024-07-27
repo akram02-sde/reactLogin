39b4bdb8f18d308db019dcf94082f178feed1e7a
Akram Ahmed Khan <akram02april@gmail.com>
2024-07-27 19:43:35 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AllRoutes from './components/AllRoutes'
+// import Login from './components/Login'
+
+function App() {
+
+  return (
+    <div className='App'>
+      {
+       <AllRoutes/>
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = ({setIsLoggedIn}) => {
+  let navigate = useNavigate();
+  const [usernameState, setUsernameState] = useState("");
+  const [passwordState, setPasswordState] = useState("");
+  const [errorMsg,setErrMsg]=useState("");
+
+  const handleLogin = () => {
+    fetch('https://dummyjson.com/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username:usernameState,
+        password:passwordState,
+      })
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          return res.json();
+        }
+        else{
+          let message=`${res.status} ${res.statusText}`;
+          setErrMsg(message);
+          throw new Error(message);
+        }
+      })
+      .then((data) =>{
+        setIsLoggedIn(true);
+          localStorage.setItem("id",data.id);
+          localStorage.setItem("token",data.token);
+          navigate(`/user/${data.id}`);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="Login">
+      <h1>Login</h1>
+    <input
+      type="text"
+      value={usernameState}
+      onChange={(e) => setUsernameState(e.target.value)}
+      placeholder="username"
+    />
+    <input
+      type="password"
+      value={passwordState}
+      onChange={(e) => setPasswordState(e.target.value)}
+      placeholder="password"
+    />
+    <button type="button" onClick={handleLogin}>
+      Login
+    </button>
+    <p className="errorMessage">{errorMsg}</p>
+  </div>
+  );
+};
+
+export default Login;
